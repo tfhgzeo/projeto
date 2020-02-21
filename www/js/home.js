1b7e757c8eb24c6f49155ca914cbdbed2bfe8c12
tfhgzeo <gustavoacm06@gmail.com>
2020-02-20 21:00:35 -0300

    Atualizacao do Firestore adiconando informações ao card de produto

--- www/js/home.js
+++ www/js/home.js
@@ -10,6 +10,12 @@ for (var i = 0; i < card.length; i++) {
 
 var db = firebase.firestore();
 
+db.collection("produtos").doc("Teste")
+    .onSnapshot(function (doc) {
+        console.log(" Nome do Produto: ", doc.data().nome);
+    });
+
+
 var produtosRef = db.collection("produtos");
 
 produtosRef.where("nome", "==", "Iphone 11")
@@ -17,7 +23,14 @@ produtosRef.where("nome", "==", "Iphone 11")
     .then(function (snapshot) {
         snapshot.forEach(function (doc) {
             console.log(doc.id, ' => ', doc.data());
-            document.querySelector(".card-title").textContent = doc.data();
+            document.querySelector(".card-title").textContent = doc.data().nome;
+            var img = document.querySelector(".card-img");
+            var imgUrl = doc.data().img;
+            var descricao = doc.data().descricao;
+            console.log(doc.data().img);
+            var card_description = document.querySelector(".card-text");
+            card_description.textContent = descricao;
+            img.src = imgUrl;
         })
     })
     .catch(function (error) {
